da8ab2e60052af461c9f8b47f924a547c231e746
Vivek <vivekananda.panigrahy@sap.com>
2019-02-06 12:30:42 +0000

    All files

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("com.s4hana.ext.ctrltable.v2.global_ctrl_table_v2.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("com.s4hana.ext.ctrltable.v2.global_ctrl_table_v2.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
